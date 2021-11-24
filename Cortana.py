b9d3111958c65d39a4a6a9b224f965342c95246b
GouthaamHarsha <94789758+GouthaamHarsha@users.noreply.github.com>
2021-11-24 11:03:55 +0530

    create cortana

---
+++ Cortana.py
@@ -0,0 +1,41 @@
+- 👋 Hi, I’m @GouthaamHarsha
+- 👀 I’m interested in coding...
+- 🌱 I’m currently learning ...
+- 📫 How to reach me ...
+
+<!---
+GouthaamHarsha/GouthaamHarsha is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+\open google/
+use code webcam/
+(python.py)
+/py.webcam/innitial,ip/
+/(follow user instructions)/
+/py.example- open google)/
+pls use deb 
+/py.initialytpro./
+py/nomenate voting/Python/Dcc
+python/download
+Dcc/download
+ self.db_save(self.your_name)      
+        self.startDocumentation(self.your_name)
+        main/Voice List.csv
+        main/Voice List.csv
+        
+   print("Processing, it will take less than a minute...")
+        os.system("python ../main/main.py")
+
+es-US-Standard-C,es-US,1,24000
+hi-IN-Standard-C,hi-IN,1,24000
+kn-IN-Standard-A,kn-IN,2,24000
+kn-IN-Standard-B,kn-IN,1,24000
+ml-IN-Standard-A,ml-IN,2,24000
+ml-IN-Standard-B,ml-IN,1,24000
+ta-IN-Standard-A,ta-IN,2,24000
+ta-IN-Standard-B,ta-IN,1,24000
+te-IN-Standard-A,te-IN,2,24000
+te-IN-Standard-B,te-IN,1,24000
+pa-IN-Standard-A,pa-IN,2,24000
+pa-IN-Standard-B,pa-IN,1,24000
+pa-IN-Standard-C,pa-IN,2,24000
